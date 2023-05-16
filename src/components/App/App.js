e45adf43eb83a8df4fc4f17ff4add6dda7d80b15
Stephen Wooten II <stephen.netu@gmail.com>
2023-05-16 11:21:11 -0400

    Using MUI

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from '@mui/material';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="contained">Hello World</Button>
+    </div>
+  );
+}
+
+export default App;
